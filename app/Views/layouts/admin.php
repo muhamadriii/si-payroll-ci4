76a4dc5633bc81d8850fd1203a19c3835e8c056d
muhmad riandi <muhamadriandi0503@gmail.com>
2025-11-30 01:54:52 +0700

    refactor(layouts): switch from local to CDN for bootstrap and jquery
    Improve page load performance by using CDN versions of bootstrap and jquery instead of local files

--- app/Views/layouts/admin.php
+++ app/Views/layouts/admin.php
@@ -100,9 +100,9 @@
     </div>
   </div>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="<?= base_url('vendor/jquery/jquery.min.js') ?>"></script>
-  <script src="<?= base_url('vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+  <!-- Bootstrap core JavaScript (CDN) -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
   <script src="<?= base_url('vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
